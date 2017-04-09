ca2b088e8837e9aed930b8690b7255dda84b6b63
TestOodoO <prakashsankark@gmail.com>
2017-04-10 03:07:50 +0530

    remaining time added

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 $a = array_fill(1, 100, 'banana');
-$cpm = 2;
+$cpm = 3;
 $i = 0;
 $start = microtime(true);
 
@@ -15,8 +15,12 @@ while(true)
 		if($i % $cpm == 0){
 			echo "Love all\n";
 			$end = microtime(true);
-			$execution_time = ($end - $start);
+			$execution_time = ($end - $start)/60;
+			$remaining_time = round(60 - $execution_time);
+			echo $remaining_time;
+			echo "\n";
 			echo $execution_time;
+			sleep($remaining_time);
 			$start = microtime(true);
 			echo "\n";
 		}
